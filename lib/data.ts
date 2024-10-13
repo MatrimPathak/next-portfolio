22a4c7b3394cefbaf41111be017b32d70e7f8498
Matrim Pathak <matrimpathak1999@gmail.com>
2024-10-14 00:50:24 +0530

    Added my projects in the list

--- lib/data.ts
+++ lib/data.ts
@@ -2,8 +2,8 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
+import portfolio from "@/public/portfolio.png";
+import carePulse from "@/public/carePulse.png";
 import wordanalyticsImg from "@/public/wordanalytics.png";
 
 export const links = [
@@ -62,25 +62,41 @@ export const experiencesData = [
 
 export const projectsData = [
 	{
-		title: "CorpComment",
+		title: "Personal Portfolio Website",
 		description:
-			"I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-		tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-		imageUrl: corpcommentImg,
+			"Developed a personal portfolio website with stunning animations, dynamic Firebase data display, and a contact form enabling user replies.",
+		tags: ["React", "Next.js", "Firebase", "TailwindCSS", "Framer Motion"],
+		imageUrl: portfolio,
 	},
+	// {
+	// 	title: "FlowBoard",
+	// 	description:
+	// 		"Developed FlowBoard, a project management app with search, team pages, light/dark mode, and real-time data visualization, using Next.js, Redux, AWS, and PostgreSQL.",
+	// 	tags: [
+	// 		"Next.js",
+	// 		"Redux",
+	// 		"TailwindCSS",
+	// 		"AWS",
+	// 		"PostgreSQL",
+	// 		"Recharts",
+	// 	],
+	// 	imageUrl: wordanalyticsImg,
+	// },
 	{
-		title: "rmtDev",
+		title: "CarePulse",
 		description:
-			"Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-		tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-		imageUrl: rmtdevImg,
-	},
-	{
-		title: "Word Analytics",
-		description:
-			"A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-		tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-		imageUrl: wordanalyticsImg,
+			"Developed CarePulse, a healthcare management system for doctor appointments with SMS notifications, admin panel, and advanced UI using Next.js, TypeScript, and TailwindCSS.",
+		tags: [
+			"React",
+			"TypeScript",
+			"Next.js",
+			"TailwindCSS",
+			"React Hook Form",
+			"Appwrite",
+			"Twilio",
+			"Sentry",
+		],
+		imageUrl: carePulse,
 	},
 ] as const;
 
